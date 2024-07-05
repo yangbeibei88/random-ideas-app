e3083ffd22dac1c83c09dbaee8d674bf0e8b345b
BeibeiYang <chrisyangber@hotmail.com>
2024-07-06 05:09:17 +1000

    database queries

--- controllers/ideaController.js
+++ controllers/ideaController.js
@@ -1,103 +1,75 @@
-let ideas = [
-  {
-    id: 1,
-    description: "Develop a mobile app to track daily fitness activities",
-    tags: ["fitness", "mobile", "app"],
-    username: "fitguru",
-    date: "2024-07-05",
-  },
-  {
-    id: 2,
-    description:
-      "Create a platform for freelance photographers to showcase their work",
-    tags: ["photography", "freelance", "portfolio"],
-    username: "photoenthusiast",
-    date: "2024-07-05",
-  },
-  {
-    id: 3,
-    description: "Design an AI-based tool for automated content generation",
-    tags: ["AI", "content", "automation"],
-    username: "techwizard",
-    date: "2024-07-05",
-  },
-  {
-    id: 4,
-    description: "Build a website for booking and reviewing local tour guides",
-    tags: ["travel", "booking", "reviews"],
-    username: "travelbuff",
-    date: "2024-07-05",
-  },
-  {
-    id: 5,
-    description:
-      "Launch an online course for learning digital marketing strategies",
-    tags: ["education", "marketing", "online course"],
-    username: "marketpro",
-    date: "2024-07-05",
-  },
-];
+import { Idea } from "../models/Idea.js";
 
-export const getIdeas = (req, res) => {
-  res.status(200).json({ success: true, data: ideas });
-};
-
-export const getIdea = (req, res) => {
-  const idea = ideas.find((idea) => idea.id === +req.params.id);
-
-  if (!idea) {
-    return res
-      .status(404)
-      .json({ success: false, error: "Resource Not Found" });
+// get all ideas
+export const getIdeas = async (req, res) => {
+  try {
+    const ideas = await Idea.find();
+    res.status(200).json({ success: true, data: ideas });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ success: false, error: "Something went wrong" });
   }
-  res.status(200).json({ success: true, data: idea });
 };
 
-export const createIdea = (req, res) => {
-  const newIdea = {
-    id: ideas.length + 1,
+// get one idea
+export const getIdea = async (req, res) => {
+  try {
+    const idea = await Idea.findById(req.params.id);
+    console.log(idea);
+    res.status(200).json({ success: true, data: idea });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ success: false, error: "Something went wrong" });
+  }
+};
+
+// create new idea
+export const createIdea = async (req, res) => {
+  // instantiate new Idea
+  const newIdea = new Idea({
     description: req.body.description,
     tags: req.body.tags.split(","),
     username: req.body.username,
-    date: new Date().toISOString().slice(0, 10),
-  };
+  });
 
-  console.log(newIdea);
-  ideas.push(newIdea);
-  res.status(201).json({ success: true, data: newIdea });
+  try {
+    const savedNewIdea = await newIdea.save();
+    console.log(savedNewIdea);
+    res.status(201).json({ success: true, data: savedNewIdea });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ success: false, error: "Something went wrong" });
+  }
 };
 
-export const updateIdea = (req, res) => {
-  const idea = ideas.find((idea) => idea.id === +req.params.id);
-
-  if (!idea) {
-    return res
-      .status(404)
-      .json({ success: false, error: "Resource Not Found" });
+// update an idea
+export const updateIdea = async (req, res) => {
+  try {
+    const updatedIdea = await Idea.findByIdAndUpdate(
+      req.params.id,
+      {
+        $set: {
+          description: req.body.description,
+          tags: req.body.tags.split(","),
+        },
+      },
+      { new: true }
+    );
+    console.log(updatedIdea);
+    res.status(200).json({ success: true, data: updatedIdea });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ success: false, error: "Something went wrong" });
   }
-
-  for (const key in idea) {
-    if (key !== "id" && key !== "username") {
-      if (Array.isArray(idea[key])) {
-        idea[key] = req.body[key].split(",") || idea[key];
-      } else {
-        idea[key] = req.body[key] || idea[key];
-      }
-    }
-  }
-  console.log(idea);
-  res.status(200).json({ success: true, data: idea });
 };
 
-export const deleteIdea = (req, res) => {
-  const idea = ideas.find((idea) => idea.id === +req.params.id);
-  if (!idea) {
-    return res
-      .status(404)
-      .json({ success: false, error: "Resource Not Found" });
+// delete an idea
+export const deleteIdea = async (req, res) => {
+  try {
+    await Idea.findByIdAndDelete(req.params.id);
+    res.status(200).json({ success: true, data: {} });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ success: false, error: "Something went wrong" });
   }
-
-  ideas = ideas.filter((idea) => idea.id !== +req.params.id);
-
-  res.status(200).json({ success: true, data: ideas });
 };
