4e29ac4442ebc001fc7e4d3b4c9f43caa9f8f147
BeibeiYang <chrisyangber@hotmail.com>
2024-07-06 15:16:35 +1000

    Modal Component

---
+++ client/src/components/Modal.js
@@ -0,0 +1,26 @@
+export class Modal {
+  constructor() {
+    this._modal = document.getElementById("modal");
+    this._modalBtn = document.getElementById("modal-btn");
+    this.addEventListeners();
+  }
+
+  addEventListeners() {
+    this._modalBtn.addEventListener("click", this.open.bind(this));
+    window.addEventListener("click", this.outsideClickHandler.bind(this));
+  }
+
+  open() {
+    this._modal.style.display = "block";
+  }
+
+  close() {
+    this._modal.style.display = "none";
+  }
+
+  outsideClickHandler(e) {
+    if (e.target === this._modal) {
+      this.close();
+    }
+  }
+}
--- client/src/index.js
+++ client/src/index.js
@@ -1,2 +1,5 @@
 import "@fortawesome/fontawesome-free/css/all.css";
 import "./css/style.css";
+import { Modal } from "./components/Modal";
+
+const modal = new Modal();
